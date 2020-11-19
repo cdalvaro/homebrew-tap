fdf771a16ce6cc30be4c37ed1e626dfa7423c961
Carlos Álvaro <carlos@cdalvaro.io>
2020-11-19 16:33:47 +0100

    audit: cpp-zmq

--- Formula/cpp-zmq.rb
+++ Formula/cpp-zmq.rb
@@ -4,10 +4,9 @@ class CppZmq < Formula
   url      "https://github.com/zeromq/cppzmq/archive/v4.7.1.tar.gz"
   sha256   "9853e0437d834cbed5d3c223bf1d755cadee70e7c964c6e42c4c6783dee5d02c"
   head     "https://github.com/zeromq/cppzmq.git"
-  bottle   :unneeded
 
   depends_on "cmake" => :build
-  depends_on "zmq"
+  depends_on "zeromq"
 
   def install
     system "cmake", ".", *std_cmake_args
