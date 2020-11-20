22dc4078f6da9bf4c853e484a9634e40065e4404
Carlos Álvaro <carlos@cdalvaro.io>
2020-11-20 11:46:24 +0100

    feat (wxmac): Enable WebView

--- Formula/wxmac.rb
+++ Formula/wxmac.rb
@@ -4,6 +4,7 @@ class Wxmac < Formula
   url      "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxWidgets-3.1.4.tar.bz2"
   sha256   "3ca3a19a14b407d0cdda507a7930c2e84ae1c8e74f946e0144d2fa7d881f1a94"
   license  "wxWindows"
+  revision 1
   head     "https://github.com/wxWidgets/wxWidgets.git"
 
   livecheck do
@@ -41,6 +42,7 @@ class Wxmac < Formula
       "--enable-svg",
       "--enable-unicode",
       "--enable-webkit",
+      "--enable-webview",
       "--with-expat",
       "--with-libjpeg",
       "--with-libpng",
