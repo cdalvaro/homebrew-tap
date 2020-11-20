16347b266c4d2e1884b3aaf487a5acd20d6f63e3
Carlos Álvaro <carlos@cdalvaro.io>
2020-11-20 08:24:18 +0100

    audit: wxmac

--- Formula/wxmac.rb
+++ Formula/wxmac.rb
@@ -4,7 +4,6 @@ class Wxmac < Formula
   url      "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxWidgets-3.1.4.tar.bz2"
   sha256   "3ca3a19a14b407d0cdda507a7930c2e84ae1c8e74f946e0144d2fa7d881f1a94"
   head     "https://github.com/wxWidgets/wxWidgets.git"
-  bottle   :unneeded
 
   option "with-enable-abort", "apply patch patch-make-public-enable-abort"
 
