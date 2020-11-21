7693a6a787943f1ef5ec2cd3a0722b411e80aefd
Carlos Álvaro <carlos@cdalvaro.io>
2020-11-21 19:48:04 +0100

    simple-web-server: Add license info

--- Formula/simple-web-server.rb
+++ Formula/simple-web-server.rb
@@ -3,6 +3,7 @@ class SimpleWebServer < Formula
   homepage "https://gitlab.com/eidheim/Simple-Web-Server"
   url      "https://gitlab.com/eidheim/Simple-Web-Server/-/archive/v3.1.1/Simple-Web-Server-v3.1.1.tar.gz"
   sha256   "f8f656d941647199e0a2db3cb07788b0e8c30d0f019d28e6ee9281bc48db132d"
+  license  "MIT"
   head     "https://gitlab.com/eidheim/Simple-Web-Server.git"
 
   depends_on "cmake" => :build
