f7b95d133bbd4c933aafaade18dab3a4740c6c3f
Carlos D. Álvaro <github@cdalvaro.io>
2021-01-29 08:17:46 +0000

    catboost: add 0.24.4 bottle.

--- Formula/catboost.rb
+++ Formula/catboost.rb
@@ -10,6 +10,12 @@ class Catboost < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/cdalvaro/homebrew-tap/releases/download/catboost-0.24.4"
+    sha256 cellar: :any_skip_relocation, big_sur: "3af9367ed2b86ccdbfc51c73e0a039036f8cb2c3942913c986da922662fe5bd1"
+    sha256 cellar: :any_skip_relocation, catalina: "4d25cdb8eca725f6334fe56714b9fad05207804075201aaa147444bfde3c3c09"
+  end
+
   resource "yandex" do
     url "https://storage.mds.yandex.net/get-devtools-opensource/471749/1903567586"
     sha256 "cb97bd28c8205c18bf9426b9457616e2fd05121562c72dcbba1ee7eca53a5034"
