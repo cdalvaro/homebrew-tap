6cdf881c722051fa09b865d495bf10ff37768728
Carlos D. Álvaro <github@cdalvaro.io>
2021-09-19 09:32:31 +0000

    cpp-zmq: update 4.8.1 bottle.

--- Formula/cpp-zmq.rb
+++ Formula/cpp-zmq.rb
@@ -12,8 +12,9 @@ class CppZmq < Formula
   end
 
   bottle do
-    root_url "https://github.com/cdalvaro/homebrew-tap/releases/download/cpp-zmq-4.7.1"
-    sha256 cellar: :any_skip_relocation, catalina: "a9de5d03bfe7d0456590792d4ba569ed2186358c023adf483a36be93916fcfce"
+    root_url "https://github.com/cdalvaro/homebrew-tap/releases/download/cpp-zmq-4.8.1"
+    sha256 cellar: :any_skip_relocation, big_sur:  "d10f518e98d3edd8734d9b41151de22e580b3239afff7eb8e59e5875d2ae0011"
+    sha256 cellar: :any_skip_relocation, catalina: "d39e23d23e1f255733ce7eb0e1cd5ca5b1bb3d6ded09446f3c334cbac6326814"
   end
 
   depends_on "cmake" => :build
