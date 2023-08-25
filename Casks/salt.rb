016bdfced30bb8a874b4001413c16797147b463f
Carlos Álvaro <carlos@cdalvaro.io>
2023-08-25 12:07:07 +0200

    cask(salt): Improve PATH config

--- Casks/salt.rb
+++ Casks/salt.rb
@@ -12,7 +12,7 @@ cask "salt" do
 
   livecheck do
     url "https://repo.saltproject.io/salt/py3/macos/latest"
-    regex(%r{salt-(\d+(?:\.\d+)?(?:-\d+)?)-py3-#{arch}\.pkg})
+    regex(/salt-(\d+(?:\.\d+)?(?:-\d+)?)-py3-#{arch}\.pkg/)
   end
 
   conflicts_with formula: "salt"
@@ -21,6 +21,7 @@ cask "salt" do
   pkg "salt-#{version}-py3-#{arch}.pkg"
 
   postflight do
+    random_str = (0...8).map { rand(65..90).chr }.join
     %w[api master minion syndic].each do |daemon|
       plist_file = "/Library/LaunchDaemons/com.saltstack.salt.#{daemon}.plist"
       xml, = system_command! "plutil",
@@ -29,16 +30,13 @@ cask "salt" do
       xml = Plist.parse_xml(xml)
 
       xml["EnvironmentVariables"] = {} unless xml.key?("EnvironmentVariables")
-
-      xml["EnvironmentVariables"]["PATH"] = if xml["EnvironmentVariables"].key?("PATH")
-        "#{HOMEBREW_PREFIX}/bin:#{xml["EnvironmentVariables"]["PATH"]}"
-      else
-        "#{HOMEBREW_PREFIX}/bin"
-      end
-
       xml["EnvironmentVariables"]["HOMEBREW_PREFIX"] = HOMEBREW_PREFIX.to_s
 
-      new_plist_file = "/tmp/#{File.basename(plist_file)}"
+      path = xml["EnvironmentVariables"]["PATH"] || "/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+      path = "#{HOMEBREW_PREFIX}/bin:#{path}" unless path.split(":").include?("#{HOMEBREW_PREFIX}/bin")
+      xml["EnvironmentVariables"]["PATH"] = path
+
+      new_plist_file = "/tmp/#{random_str}.#{File.basename(plist_file)}"
       File.write(new_plist_file, xml.to_plist)
       system_command! "plutil",
                       args: ["-lint", new_plist_file]
@@ -47,7 +45,7 @@ cask "salt" do
                       args: [new_plist_file, plist_file],
                       sudo: true
       system_command! "chown",
-                      args: ["root:wheel", "plist_file"],
+                      args: ["root:wheel", plist_file],
                       sudo: true
     end
   end
