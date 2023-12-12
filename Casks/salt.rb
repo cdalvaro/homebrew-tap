97ffd3933524aecaa263e42a335bcfa5c73271bd
Carlos Álvaro <carlos@cdalvaro.io>
2023-12-12 23:57:08 +0100

    salt(cask): 3006.5
    Add support for Apple Silicon

--- Casks/salt.rb
+++ Casks/salt.rb
@@ -1,12 +1,11 @@
 cask "salt" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3006.4"
-  sha256 arm:   "4c624e7451bd58c6364863087dba9f425e0bc0373984cb808430e7fbeac069a5",
-         intel: "4c624e7451bd58c6364863087dba9f425e0bc0373984cb808430e7fbeac069a5"
+  version "3006.5"
+  sha256 arm:   "bac43fcb5ca04679245e1b6510cb060f282c6f1bb15280396214ca671c74ce65",
+         intel: "90c55ae9836d7f3f1e2c6cb930cff60b334393c87482f26b32121c04db2ad5ea"
 
-  # url "https://repo.saltproject.io/salt/py3/macos/minor/#{version}/salt-#{version}-py3-#{arch}.pkg"
-  url "https://repo.saltproject.io/salt/py3/macos/minor/#{version}/salt-#{version}-py3-x86_64.pkg"
+  url "https://repo.saltproject.io/salt/py3/macos/minor/#{version}/salt-#{version}-py3-#{arch}.pkg"
   name "Salt"
   desc "World's fastest, most intelligent and scalable automation engine"
   homepage "https://saltproject.io/"
