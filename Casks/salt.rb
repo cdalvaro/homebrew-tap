ad10d493db0692e8dba1ab086deb4de9a94280db
Carlos Álvaro <github@cdalvaro.io>
2024-02-01 14:33:57 +0100

    salt(cask): Fix Layout/ArgumentAlignment offenses

--- Casks/salt.rb
+++ Casks/salt.rb
@@ -24,8 +24,8 @@ cask "salt" do
     %w[api master minion syndic].each do |daemon|
       plist_file = "/Library/LaunchDaemons/com.saltstack.salt.#{daemon}.plist"
       xml, = system_command "plutil",
-                             args: ["-convert", "xml1", "-o", "-", "--", plist_file],
-                             sudo: true
+                            args: ["-convert", "xml1", "-o", "-", "--", plist_file],
+                            sudo: true
       xml = Plist.parse_xml(xml)
 
       xml["EnvironmentVariables"] = {} unless xml.key?("EnvironmentVariables")
@@ -39,14 +39,14 @@ cask "salt" do
       new_plist_file = "/tmp/#{random_str}.#{File.basename(plist_file)}"
       File.write(new_plist_file, xml.to_plist)
       system_command "plutil",
-                      args: ["-lint", new_plist_file]
+                     args: ["-lint", new_plist_file]
 
       system_command "mv",
-                      args: [new_plist_file, plist_file],
-                      sudo: true
+                     args: [new_plist_file, plist_file],
+                     sudo: true
       system_command "chown",
-                      args: ["root:wheel", plist_file],
-                      sudo: true
+                     args: ["root:wheel", plist_file],
+                     sudo: true
     end
   end
 
