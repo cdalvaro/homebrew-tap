d41d63ee8c5220913c892df3671468f29cf701c8
Carlos Álvaro <github@cdalvaro.io>
2024-02-02 12:42:48 +0100

    Revert "ref: Move Utils module to the end of file"
    This reverts commit 2c5cc40883e05bc335c5a05573c2fa71ba8bcbd2.

--- Casks/salt.rb
+++ Casks/salt.rb
@@ -1,3 +1,34 @@
+module Utils
+  def self.patch_plist(daemon)
+    plist_file = "/Library/LaunchDaemons/com.saltstack.salt.#{daemon}.plist"
+    xml, = system_command "plutil",
+                          args: ["-convert", "xml1", "-o", "-", "--", plist_file],
+                          sudo: true
+    xml = Plist.parse_xml(xml)
+
+    xml["EnvironmentVariables"] = {} unless xml.key?("EnvironmentVariables")
+    xml["EnvironmentVariables"]["HOMEBREW_PREFIX"] = HOMEBREW_PREFIX.to_s
+    xml["EnvironmentVariables"]["HOME"] ||= "/var/root"
+
+    path = xml["EnvironmentVariables"]["PATH"] || "/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    path = "#{HOMEBREW_PREFIX}/bin:#{path}" unless path.split(":").include?("#{HOMEBREW_PREFIX}/bin")
+    xml["EnvironmentVariables"]["PATH"] = path
+
+    random_str = (0...8).map { rand(65..90).chr }.join
+    new_plist_file = "/tmp/#{random_str}.#{File.basename(plist_file)}"
+    File.write(new_plist_file, xml.to_plist)
+    system_command "plutil",
+                   args: ["-lint", new_plist_file]
+
+    system_command "mv",
+                   args: [new_plist_file, plist_file],
+                   sudo: true
+    system_command "chown",
+                   args: ["root:wheel", plist_file],
+                   sudo: true
+  end
+end
+
 cask "salt" do
   arch arm: "arm64", intel: "x86_64"
 
@@ -42,36 +73,5 @@ cask "salt" do
       sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.minion.plist
       sudo launchctl load -w /Library/LaunchDaemons/com.saltstack.salt.syndic.plist
     CAVEATS
-  end
-end
-
-module Utils
-  def self.patch_plist(daemon)
-    plist_file = "/Library/LaunchDaemons/com.saltstack.salt.#{daemon}.plist"
-    xml, = system_command "plutil",
-                          args: ["-convert", "xml1", "-o", "-", "--", plist_file],
-                          sudo: true
-    xml = Plist.parse_xml(xml)
-
-    xml["EnvironmentVariables"] = {} unless xml.key?("EnvironmentVariables")
-    xml["EnvironmentVariables"]["HOMEBREW_PREFIX"] = HOMEBREW_PREFIX.to_s
-    xml["EnvironmentVariables"]["HOME"] ||= "/var/root"
-
-    path = xml["EnvironmentVariables"]["PATH"] || "/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-    path = "#{HOMEBREW_PREFIX}/bin:#{path}" unless path.split(":").include?("#{HOMEBREW_PREFIX}/bin")
-    xml["EnvironmentVariables"]["PATH"] = path
-
-    random_str = (0...8).map { rand(65..90).chr }.join
-    new_plist_file = "/tmp/#{random_str}.#{File.basename(plist_file)}"
-    File.write(new_plist_file, xml.to_plist)
-    system_command "plutil",
-                   args: ["-lint", new_plist_file]
-
-    system_command "mv",
-                   args: [new_plist_file, plist_file],
-                   sudo: true
-    system_command "chown",
-                   args: ["root:wheel", plist_file],
-                   sudo: true
   end
 end
