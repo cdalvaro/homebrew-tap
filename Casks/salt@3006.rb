5adc1b16bbd5b694af43e8dc8a07d425ec3ec2e8
Carlos Álvaro <carlos@cdalvaro.io>
2024-11-23 14:19:24 +0100

    fix: Do not use lib module

--- Casks/salt@3006.rb
+++ Casks/salt@3006.rb
@@ -1,3 +1,39 @@
+module Utils
+  extend SystemCommand::Mixin
+
+  def self.patch_plist(daemon)
+    plist_file = "/Library/LaunchDaemons/com.saltstack.salt.#{daemon}.plist"
+    xml, _, status = system_command "plutil",
+                                    args: ["-convert", "xml1", "-o", "-", "--", plist_file],
+                                    sudo: true
+
+    return unless status.success?
+
+    xml = Plist.parse_xml(xml)
+
+    xml["EnvironmentVariables"] = {} unless xml.key?("EnvironmentVariables")
+    xml["EnvironmentVariables"]["HOMEBREW_PREFIX"] = HOMEBREW_PREFIX.to_s
+    xml["EnvironmentVariables"]["HOME"] ||= "/var/root"
+
+    path = xml["EnvironmentVariables"]["PATH"] || "/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    path = "#{HOMEBREW_PREFIX}/bin:#{path}" unless path.split(":").include?("#{HOMEBREW_PREFIX}/bin")
+    xml["EnvironmentVariables"]["PATH"] = path
+
+    random_str = (0...8).map { rand(65..90).chr }.join
+    new_plist_file = "/tmp/#{random_str}.#{File.basename(plist_file)}"
+    File.write(new_plist_file, xml.to_plist)
+    system_command "plutil",
+                   args: ["-lint", new_plist_file]
+
+    system_command "mv",
+                   args: [new_plist_file, plist_file],
+                   sudo: true
+    system_command "chown",
+                   args: ["root:wheel", plist_file],
+                   sudo: true
+  end
+end
+
 cask "salt@3006" do
   arch arm: "arm64", intel: "x86_64"
 
@@ -22,7 +58,7 @@ cask "salt@3006" do
 
   postflight do
     require_relative "#{`brew --repository cdalvaro/tap`.lines.first.chomp}/lib/patches/salt"
-    %w[api master minion syndic].each { |daemon| Patches::Salt.patch_plist(daemon) }
+    %w[api master minion syndic].each { |daemon| Utils.patch_plist(daemon) }
   end
 
   uninstall launchctl: [
