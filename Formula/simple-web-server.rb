80f78ef24f84ee3ed18331b33e2e83037a72812f
Carlos D. Álvaro <16347742+cdalvaro@users.noreply.github.com>
2025-01-06 13:54:27 +0000

    simple-web-server: update 3.1.1_1 bottle.

--- Formula/simple-web-server.rb
+++ Formula/simple-web-server.rb
@@ -8,8 +8,11 @@ class SimpleWebServer < Formula
   head     "https://gitlab.com/eidheim/Simple-Web-Server.git"
 
   bottle do
-    root_url "https://github.com/cdalvaro/homebrew-tap/releases/download/simple-web-server-3.1.1"
-    sha256 cellar: :any_skip_relocation, catalina: "7f5d751fcd82dfa93cdd266a10686592b855bf0ba445ef6c389b0c87b1adbb37"
+    root_url "https://ghcr.io/v2/cdalvaro/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "74e8cdc04779612c8a2cf12c4fcc5996feb2706438a2397aaed8fe16f3777107"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b0cb4fd948065163412e1edfaaf573f1d88fd5c17dd9a93fc3f077fb390aac21"
+    sha256 cellar: :any_skip_relocation, ventura:       "188770270aaec21f026833ee532d1bb4d32bd4f8b40181d9192922887d9869e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e99f78eee08cec869e258a20fa032e1662a691e2a38bc6ecfae380c9f8a7fd9"
   end
 
   depends_on "cmake" => :build
