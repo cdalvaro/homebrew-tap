2d62908d2358b9f83d3db487e6de3f11e584540b
Carlos Álvaro <carlos@cdalvaro.io>
2025-05-17 08:39:05 +0200

    catboostmodel-cpp: 1.2.8

--- Formula/catboostmodel-cpp.rb
+++ Formula/catboostmodel-cpp.rb
@@ -1,25 +1,9 @@
-require "yaml"
-
-module ::Utils
-  def self.add_clang_version_to_conan_settings(version)
-    system "conan", "config", "init"
-    conan_home = safe_popen_read("conan", "config", "home").strip
-    settings_file = "#{conan_home}/settings.yml"
-    settings = YAML.load_file(settings_file, aliases: true)
-    clang_versions = settings["compiler"]["clang"]["version"]
-    unless clang_versions.include?(version)
-      clang_versions << version
-      File.write(settings_file, YAML.dump(settings))
-    end
-  end
-end
-
 class CatboostmodelCpp < Formula
   desc "Gradient Boosting on Decision Trees C++ Model Library"
   homepage "https://catboost.ai"
   url "https://github.com/catboost/catboost.git",
-    tag:      "v1.2.7",
-    revision: "f903943a8cd903a117c3d3c8421cc72d3910562c"
+    tag:      "v1.2.8",
+    revision: "0bcf252505e3d1cf01acd925dcd7026799512fb9"
   license "Apache-2.0"
   head "https://github.com/catboost/catboost.git", branch: "master"
 
@@ -35,35 +19,56 @@ class CatboostmodelCpp < Formula
   option "with-static", "Also install the static library"
 
   depends_on "cmake" => :build
-  depends_on "conan@1" => :build
+  depends_on "conan" => :build
   depends_on "ninja" => :build
 
   uses_from_macos "llvm" => :build
 
   on_linux do
+    depends_on "openssl@3.0" => :build
     depends_on "lld"
+
+    patch :DATA
   end
 
   resource "model.cbm" do
-    url "https://github.com/catboost/catboost/raw/refs/tags/v1.2.7/catboost/jvm-packages/catboost4j-prediction/src/test/resources/models/model.cbm"
+    url "https://github.com/catboost/catboost/raw/refs/tags/v1.2.8/catboost/jvm-packages/catboost4j-prediction/src/test/resources/models/model.cbm"
     sha256 "9b07d25cf49f0e30ebcc05b7545af5fe029fb3f432a4f101949e4e48a357e164"
   end
 
   def install
-    Utils.add_clang_version_to_conan_settings(Formula["llvm"].version.major.to_s) if ENV.key?("GITHUB_ACTIONS")
+    # Replace openssl::openssl by OpenSSL::SSL
+    # Otherwise target_link_libraries fails
+    Dir.glob("**/CMakeLists.*.txt") do |file|
+      content = File.read(file)
+      if content.include?("openssl::openssl")
+        content.gsub!("openssl::openssl", "OpenSSL::SSL")
+        File.write(file, content)
+      end
+    end
+
+    cmake_project_top_level_includes = ["#{buildpath}/cmake/conan_provider.cmake"]
+
+    # Check if CMAKE_PROJECT_TOP_LEVEL_INCLUDES is already specified in std_cmake_args
+    cmake_top_level_includes = std_cmake_args.find { |arg| arg.start_with?("-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=") }
+    if cmake_top_level_includes
+      cmake_project_top_level_includes.unshift(cmake_top_level_includes.split("=")[1])
+      std_cmake_args.delete(cmake_top_level_includes)
+    end
 
     args = [
       "-DCATBOOST_COMPONENTS=libs",
       "-DHAVE_CUDA=no",
       "-DCMAKE_POSITION_INDEPENDENT_CODE=On",
       "-DCMAKE_TOOLCHAIN_FILE=#{buildpath}/build/toolchains/clang.toolchain",
+      "-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=#{cmake_project_top_level_includes.join(";")}",
     ]
 
     targets = ["catboostmodel"]
     targets << "catboostmodel_static" if build.with?("static")
 
     cmakepath = buildpath/"cmake-build"
-    system "cmake", "-S", ".", "-B", cmakepath, "-G", "Ninja", *args, *std_cmake_args
+    system "cmake", "-S", ".", "-B", cmakepath, "-G", "Ninja", *std_cmake_args, *args
     system "ninja", "-C", cmakepath, *targets
 
     lib.install cmakepath/"catboost/libs/model_interface/libcatboostmodel.#{OS.mac? ? "dylib" : "so"}"
@@ -103,3 +108,19 @@ class CatboostmodelCpp < Formula
     end
   end
 end
+
+__END__
+diff --git a/conanfile.py b/conanfile.py
+index 72453fe00a..8f4479b721 100644
+--- a/conanfile.py
++++ b/conanfile.py
+@@ -13,9 +13,6 @@ class App(ConanFile):
+
+     default_options = {}
+
+-    def requirements(self):
+-        self.requires("openssl/3.0.15")
+-
+     def build_requirements(self):
+         self.tool_requires("ragel/6.10")
+         self.tool_requires("swig/4.0.2")
