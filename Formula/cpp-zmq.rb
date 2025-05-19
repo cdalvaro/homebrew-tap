39b7a12b36c1c1645409e95055c17ffdd91ad921
Carlos D. Álvaro <16347742+cdalvaro@users.noreply.github.com>
2025-05-19 18:21:00 +0000

    cpp-zmq: update 4.10.0_1 bottle.

--- Formula/cpp-zmq.rb
+++ Formula/cpp-zmq.rb
@@ -13,10 +13,11 @@ class CppZmq < Formula
   end
 
   bottle do
-    root_url "https://github.com/cdalvaro/homebrew-tap/releases/download/cpp-zmq-4.9.0"
-    sha256 cellar: :any_skip_relocation, monterey: "135ba553cac661ac9a608e96880307852719b21e6996d7796e428dbd396f3515"
-    sha256 cellar: :any_skip_relocation, big_sur:  "76849717683fae74b865e69cbcf31e508cd91c571174d9fc0b9231290dc19fb3"
-    sha256 cellar: :any_skip_relocation, catalina: "1706b12442298413e6b1a530f782463c4a5f3f5d80daa9800b7ee0d98390a218"
+    root_url "https://ghcr.io/v2/cdalvaro/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3f177a06b2d9f1541a424d9482418491649cdc7922a3c5b3c3d275f37236f834"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1a7f74fc1708266db690c49eda545dbccba4ef534e3ba53a8f7dc22c85449c4"
+    sha256 cellar: :any_skip_relocation, ventura:       "6b220545eadfdb0273be30b0f9f6f189565513f588288f0d2730a6aa70fa8423"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b62aa613b53b1212b7c2dc8d7114e42cc9391ffba30090787a6d8beb19d1147"
   end
 
   depends_on "cmake" => :build
