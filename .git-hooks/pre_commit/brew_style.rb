fe05e25ce93e72e9e77947e5fbd24057daa30a57
Carlos Álvaro <carlos@cdalvaro.io>
2025-05-19 14:35:33 +0200

    chore: Fix style in BrewStyle

--- .git-hooks/pre_commit/brew_style.rb
+++ .git-hooks/pre_commit/brew_style.rb
@@ -1,34 +1,35 @@
 # frozen_string_literal: true
 
-module Overcommit::Hook::PreCommit
-  class BrewStyle < Base
+module Overcommit
+  module Hook
+    module PreCommit
+      class BrewStyle < Base
+        FORMAT_AUDIT_CATEGORIZER = lambda do |type|
+          type.match?(/\[correct(?:able|ed)\]/i) ? :warning : :error
+        end.freeze
 
-    FORMAT_AUDIT_CATEGORIZER = lambda do |type|
-      type.match?(/\[correct(?:able|ed)\]/i) ? :warning : :error
-    end
+        def run
+          result = execute(command, args: applicable_files)
+          return :pass if result.success? && !fix?
 
-    def run
-      result = execute(command, args: applicable_files)
-      return :pass if result.success? && !fix?
+          audit_messages = result.stdout.split("\n").grep(/^.+?:\d+:\d+:/)
+          extract_messages(
+            audit_messages,
+            /^(?<file>(?:\w:)?[^:]+):(?<line>\d+):[^ ]+ (?<type>\w:(?: \[correct(?:able|ed)\])? [^ ]+:)/i,
+            FORMAT_AUDIT_CATEGORIZER,
+          )
+        end
 
-      audit_messages = result.stdout.split("\n").select { |line| line.match?(/^.+?:\d+:\d+:/) }
-      audit_messages = extract_messages(
-        audit_messages,
-        /^(?<file>(?:\w:)?[^:]+):(?<line>\d+):[^ ]+ (?<type>\w:(?: \[correct(?:able|ed)\])? [^ ]+:)/i,
-        FORMAT_AUDIT_CATEGORIZER
-      )
+        def fix?
+          config["command"].include?("--fix") || config["flags"].include?("--fix")
+        end
 
-      audit_messages
-    end
+        def offenses_corrected?(result)
+          return false unless fix?
 
-    def fix?
-      config["command"].include?("--fix") || config["flags"].include?("--fix")
-    end
-
-    def offenses_corrected?(result)
-      return false unless fix?
-
-      result.stdout.match?(/\d+ offense(s)? corrected/)
+          result.stdout.match?(/\d+ offense(s)? corrected/)
+        end
+      end
     end
   end
 end
